58c4133d943cfc5c349375b7255ffef5a26d7c22
Matteo Di Giovinazzo <matteodg@gmail.com>
2019-02-03 23:46:14 -0700

    Fix random from np and created notebook

--- bm.py
+++ bm.py
@@ -1,14 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 %matplotlib inline
+
 steps = 5000
-random.seed(42)
+np.random.seed(42)
+
 x,y = np.cumsum(np.random.randn(steps)), np.cumsum(np.random.randn(steps))
+
 points = 10
 ip = lambda x, steps, points: np.interp(np.arange(steps*points), 
                                         np.arange(steps)*points, 
                                         x)
 X, Y = ip(x, steps, points), ip(y, steps, points)
+
 fig, ax = plt.subplots(1, 1, figsize=(10, 10))
 ax.set_title('Brownian Motion')
 ax.set_xlabel('x')
